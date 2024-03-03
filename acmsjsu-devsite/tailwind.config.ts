e558d75adc860a944867b2ac96f50ed04fe6ca45
kevintsoii <kevin.tsoi44@gmail.com>
2024-03-02 18:26:24 -0800

    add base styles

--- acmsjsu-devsite/tailwind.config.ts
+++ acmsjsu-devsite/tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "primary": "#F3C954",
+        "primary-highight": "#FDD976",
+        "secondary": "#196096",
+        "secondary-highlight": "#318BCF",
+        "accent": "#FDD976",
+        "background": "#FFFFFF",
+        "text": "#161616",
+        "neutral": "#A4A4A4",
+        "border": "#E5E5E5",
+        "border-hovered": "#FFFFFF",
+      },
     },
   },
   plugins: [],
