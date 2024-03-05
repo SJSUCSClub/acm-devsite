c3f3ff38f58fab0dddeb6cb94d43b50f630c3f52
TimothyPhan2 <98495706+TimothyPhan2@users.noreply.github.com>
2024-03-04 23:42:04 -0800

    Cleaned up code and styling

---
+++ acmsjsu-devsite/src/app/eventspage/CalendarEmbed.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CalendarEmbed = () => {
+  return (
+    <>
+      <iframe className="relative top-8"
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&bgcolor=%23ffffff"
+        style={{ border: "solid 1px #777" }}
+        width="700"
+        height="500"
+      ></iframe>
+    </>
+  );
+};
+
+export default CalendarEmbed;
--- acmsjsu-devsite/src/app/eventspage/page.tsx
+++ acmsjsu-devsite/src/app/eventspage/page.tsx
@@ -1,137 +1,143 @@
-import Image from 'next/image'
+import Image from "next/image";
+import CalendarEmbed from "./CalendarEmbed";
 
-const buttonStyle1: React.CSSProperties = {
-  color: 'Blue',
-  border: '2px solid ',
-  backgroundColor: 'white',
-  padding: '10px 20px',
-  textAlign: 'center',
-  display: 'inline-block',
-  fontSize: 16,
-  margin: '4px 2px',
-  transitionDuration: '0.4s',
-  cursor: 'pointer',
-  borderRadius: 8,
-  position: 'absolute',
-  right: '0px',
-  top: '0px',
-  transform: 'translateY(-50)',
-}
-const buttonStyle2: React.CSSProperties = {
-  color: 'White',
-  border: '2px solid',
-  backgroundColor: 'blue',
-  padding: '11px 23px',
-  textAlign: 'center',
-  display: 'inline-block',
-  fontSize: 16,
-  margin: '4px 2px',
-  transitionDuration: '0.4s',
-  cursor: 'pointer',
-  borderRadius: 8,
-  position: 'absolute',
-  right: '120px',
-  top: '0px',
-  transform: 'translateY(-50)',
-}
-const buttonStyle3: React.CSSProperties = {
-  color: 'Blue',
-  padding: '15px 32px',
-  textAlign: 'center',
-  display: 'inline-block',
-  fontSize: 16,
-  margin: '4px 2px',
-  transitionDuration: '0.4s',
-  cursor: 'pointer',
-  borderRadius: 8,
-  position: 'absolute',
-  right: '220px',
-  top: '0px',
-  transform: 'translateY(-50)',
-}
-const buttonStyle4: React.CSSProperties = {
-  color: 'Blue',
-  padding: '15px 32px',
-  textAlign: 'center',
-  display: 'inline-block',
-  fontSize: 16,
-  margin: '4px 2px',
-  transitionDuration: '0.4s',
-  cursor: 'pointer',
-  borderRadius: 8,
-  position: 'absolute',
-  right: '300px',
-  top: '0px',
-  transform: 'translateY(-50)',
-}
-const buttonStyle5: React.CSSProperties = {
-  color: 'Blue',
-  padding: '15px 32px',
-  textAlign: 'center',
-  display: 'inline-block',
-  fontSize: 16,
-  margin: '4px 2px',
-  transitionDuration: '0.4s',
-  cursor: 'pointer',
-  borderRadius: 8,
-  position: 'absolute',
-  right: '385px',
-  top: '0px',
-  transform: 'translateY(-50)',
-}
-const buttonStyle6: React.CSSProperties = {
-  color: 'Blue',
-  padding: '15px 32px',
-  textAlign: 'center',
-  display: 'inline-block',
-  fontSize: 16,
-  margin: '4px 2px',
-  transitionDuration: '0.4s',
-  cursor: 'pointer',
-  borderRadius: 8,
-  position: 'absolute',
-  right: '465px',
-  top: '0px',
-  transform: 'translateY(-50)',
-}
-const headerStyle1: React.CSSProperties = {
-  color: 'black',
-  position: 'absolute',
-  top: '50px',
-  fontSize: '30px',
-  fontWeight: 'bold',
-}
-const headerStyle2: React.CSSProperties = {
-  color: 'black',
-  position: 'absolute',
-  top: '100px',
-}
-const headerStyle3: React.CSSProperties = {
-  color: 'black',
-  position: 'absolute',
-  top: '120px',
-  fontWeight: 'bold',
-}
+// const buttonStyle1: React.CSSProperties = {
+//   color: 'Blue',
+//   border: '2px solid ',
+//   backgroundColor: 'white',
+//   padding: '10px 20px',
+//   textAlign: 'center',
+//   display: 'inline-block',
+//   fontSize: 16,
+//   margin: '4px 2px',
+//   transitionDuration: '0.4s',
+//   cursor: 'pointer',
+//   borderRadius: 8,
+//   position: 'absolute',
+//   right: '0px',
+//   top: '0px',
+//   transform: 'translateY(-50)',
+// }
+// const buttonStyle2: React.CSSProperties = {
+//   color: 'White',
+//   border: '2px solid',
+//   backgroundColor: 'blue',
+//   padding: '11px 23px',
+//   textAlign: 'center',
+//   display: 'inline-block',
+//   fontSize: 16,
+//   margin: '4px 2px',
+//   transitionDuration: '0.4s',
+//   cursor: 'pointer',
+//   borderRadius: 8,
+//   position: 'absolute',
+//   right: '120px',
+//   top: '0px',
+//   transform: 'translateY(-50)',
+// }
+// const buttonStyle3: React.CSSProperties = {
+//   color: 'Blue',
+//   padding: '15px 32px',
+//   textAlign: 'center',
+//   display: 'inline-block',
+//   fontSize: 16,
+//   margin: '4px 2px',
+//   transitionDuration: '0.4s',
+//   cursor: 'pointer',
+//   borderRadius: 8,
+//   position: 'absolute',
+//   right: '220px',
+//   top: '0px',
+//   transform: 'translateY(-50)',
+// }
+// const buttonStyle4: React.CSSProperties = {
+//   color: 'Blue',
+//   padding: '15px 32px',
+//   textAlign: 'center',
+//   display: 'inline-block',
+//   fontSize: 16,
+//   margin: '4px 2px',
+//   transitionDuration: '0.4s',
+//   cursor: 'pointer',
+//   borderRadius: 8,
+//   position: 'absolute',
+//   right: '300px',
+//   top: '0px',
+//   transform: 'translateY(-50)',
+// }
+// const buttonStyle5: React.CSSProperties = {
+//   color: 'Blue',
+//   padding: '15px 32px',
+//   textAlign: 'center',
+//   display: 'inline-block',
+//   fontSize: 16,
+//   margin: '4px 2px',
+//   transitionDuration: '0.4s',
+//   cursor: 'pointer',
+//   borderRadius: 8,
+//   position: 'absolute',
+//   right: '385px',
+//   top: '0px',
+//   transform: 'translateY(-50)',
+// }
+// const buttonStyle6: React.CSSProperties = {
+//   color: '#196096',
+//   padding: '15px 32px',
+//   textAlign: 'center',
+//   display: 'inline-block',
+//   fontSize: 16,
+//   margin: '4px 2px',
+//   transitionDuration: '0.4s',
+//   cursor: 'pointer',
+//   borderRadius: 8,
+//   position: 'absolute',
+//   right: '465px',
+//   top: '0px',
+//   transform: 'translateY(-50)',
+// }
+// const headerStyle1: React.CSSProperties = {
+//   color: 'black',
+//   position: 'absolute',
+//   top: '50px',
+//   fontSize: '30px',
+//   fontWeight: 'bold',
+// }
+// const headerStyle2: React.CSSProperties = {
+//   color: 'black',
+//   position: 'absolute',
+//   top: '100px',
+// }
+// const headerStyle3: React.CSSProperties = {
+//   color: 'black',
+//   position: 'absolute',
+//   top: '120px',
+//   fontWeight: 'bold',
+// }
 const CalendarPage = () => {
-    const embedCode = `<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&bgcolor=%23ffffff" style="border:solid 1px #777" width="700" height="500" frameborder="0" scrolling="no"></iframe>`;
-    return (
-      <div>
-        <h1 style = {headerStyle1}> Upcoming Events</h1>
-        <button style={buttonStyle1}> Register</button>
-        <button style={buttonStyle2}> Sign In</button>
-        <button style={buttonStyle3}> Contact</button>
-        <button style={buttonStyle4}> Projects</button>
-        <button style={buttonStyle5}> Events</button>
-        <button style={buttonStyle6}> About</button>
-        <h2 style = {headerStyle2}>
-          Our student group organizes a variety of events during each academic semester. Including workshops, informational sessions, community engagement activities, and much more! 
-        </h2>
-        <h3 style = {headerStyle3}>
-          These events are accessible to all those who are interested, irrespective of their major or prior experience.
-        </h3>
-      <div dangerouslySetInnerHTML={{ __html: embedCode }} /> 
+  return (
+    <>
+      <div className="flex gap-10 items-center justify-end mr-20 mt-4 font-bold">
+        <button className="text-[#196096]"> About</button>
+        <button className="text-[#196096]"> Events</button>
+        <button className="text-[#196096]"> Projects</button>
+        <button className="text-[#196096]"> Contact</button>
+        <button className="rounded-full bg-[#196096] text-white font-bold py-2 px-4">Register</button>
+        <button className=" border-2 border-[#196096] rounded-full text-[#196096] font-bold py-2 px-4"> Sign In</button>
       </div>
-    )
-}
+      <div className="flex flex-col  ml-40 space-y-4">
+      <h1 className="font-bold text-4xl"> Upcoming Events</h1>
+      <h2>
+      Our student group organizes a variety of events during each academic semester, including workshops, informational sessions, community engagement activities, and much more!
+      </h2>
+      <h3 className="font-bold">
+        These events are accessible to all those who are interested,
+        irrespective of their major or prior experience.
+      </h3>
 
-export default CalendarPage
+      </div>
+      <CalendarEmbed />
+    </>
+  );
+};
+
+export default CalendarPage;
