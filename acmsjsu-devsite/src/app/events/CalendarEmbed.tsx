5d652cbad5489888e7a56a05801d7b2781dc30f8
chrehall68 <chrehall68@gmail.com>
2025-09-05 17:10:30 -0700

    format file, change gcalendar

--- acmsjsu-devsite/src/app/events/CalendarEmbed.tsx
+++ acmsjsu-devsite/src/app/events/CalendarEmbed.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-
 const CalendarEmbed = () => {
   return (
     <>
-      <iframe className="top-8"
-        src="https://calendar.google.com/calendar/embed?src=c_f6ffdf34c1ec68eef08ebb7071896355e1323cf5a710228e65bc67738cb6e477%40group.calendar.google.com&ctz=America%2FLos_Angeles&bgcolor=%23ffffff"
+      <iframe
+        className="top-8"
+        src="https://calendar.google.com/calendar/embed?src=c_73d483091e5e5ada401c3525257e3f41c324e3109afe2ce11eeadec17f8580f6%40group.calendar.google.com&ctz=America%2FLos_Angeles"
         style={{ border: "solid 1px #777" }}
         width="700"
         height="500"
